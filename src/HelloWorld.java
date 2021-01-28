7797bcd74273e81b3ac58c49328203c04a0949a0
pdh1124 <pdw3231@naver.com>
2021-01-28 09:21:56 +0900

    자바 기초 강의 - HelloWolrd

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) { //메인 함수이며, 우선으로 실행되는 메소드
+		// TODO Auto-generated method stub
+		System.out.println("자바 기초 공부"); //로그출력. println은 줄 바꿈을 한다.
+		System.out.print("자바 "); //print은 줄 바꿈을 하지 않는다.
+		System.out.print("기초 공부"); 
+	}
+
+}
